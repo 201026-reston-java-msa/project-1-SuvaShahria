fbe7e173e0e8bba60ad87523192637c72a1c8bb5
SuvaShahria <1uknow2@gmail.com>
2020-11-23 22:15:42 -0500

    woo started

--- project-1/src/main/java/com/revature/Driver.java
+++ project-1/src/main/java/com/revature/Driver.java
@@ -1,9 +1,14 @@
 package com.revature;
 
+import org.apache.log4j.Logger;
+
 public class Driver {
 
+	private static final Logger log = Logger.getLogger(Driver.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		log.warn("Login Failed");
+		System.out.println("w");
 
 	}
 }
